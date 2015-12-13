c823f49fe1033310065f40d904d8b0c2e52bc777
justin <cong.itsoft@gmail.com>
2015-12-13 14:07:13 +0700

    test

--- sat8/Test.py
+++ sat8/Test.py
@@ -1,34 +1,20 @@
-import sys
-import os
+# SQL DATABASE SETTING
+MYSQL_SERVER = '127.0.0.1'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
+MYSQL_DB = 'search'
 
-path = os.path.dirname(os.path.realpath(__file__ + '../..'))
-sys.path.append(path)
-
-from Helpers import Functions
-
-from Posts.PostES import PostES
-from Products.DbProduct import DbProduct
-
-
-# print sys.path
-
-# Test class db
-
-p = PostES()
-print p.count('posts')
-
-
-# print es.get('posts', 5)
-
-
-# for i in post:
-# 	print post[i]
-
-# # print es.count('posts')
-
-# print es.insertOrUpdate(1, post)
-# print es.get('posts', 1)['title']
-
-# print es.update(2, post)
-# print es.get('posts', 2)
-
+# connect to the MySQL server
+try:
+	MYSQL_CONN = pymysql.connect(host=MYSQL_SERVER,
+										port=MYSQL_PORT,
+										user=MYSQL_USER,
+										password=MYSQL_PASSWORD,
+										db=MYSQL_DB,
+										charset='utf8',
+										cursorclass=pymysql.cursors.DictCursor,
+										use_unicode=True)
+except pymysql.Error, e:
+	logging.error("Error %d: %s" % (e.args[0], e.args[1]))
+	sys.exit(1)
