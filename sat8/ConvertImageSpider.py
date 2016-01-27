161216ae731f0eb66b536af2e6798b69d8030cec
justin <cong.itsoft@gmail.com>
2016-01-27 09:54:48 +0700

    content image

--- sat8/ConvertImageSpider.py
+++ sat8/ConvertImageSpider.py
@@ -59,7 +59,7 @@ class ConvertImageSpider():
       return self.text
 
 
-query = "SELECT * FROM posts WHERE has_image_content != 1 ORDER BY updated_at DESC LIMIT 0,1000"
+query = "SELECT * FROM posts WHERE has_image_content != 1 ORDER BY updated_at DESC LIMIT 1000,1000"
 cursor.execute(query)
 
 posts = cursor.fetchall()
