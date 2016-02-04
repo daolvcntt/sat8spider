c28642d7ac9d09b40a8e521cf11c47fc3d4f101c
justin <cong.itsoft@gmail.com>
2016-02-04 14:08:36 +0700

    download image product

--- sat8/settings.py
+++ sat8/settings.py
@@ -21,7 +21,8 @@ SPIDER_MODULES = ['sat8.spiders']
 NEWSPIDER_MODULE = 'sat8.spiders'
 ITEM_PIPELINES = {
 	'sat8.pipelines.MySQLStorePipeline' : 100,
-	# 'sat8.MyImagesPipeline.MyImagesPipeline' : 102,
+	# 'scrapy.pipelines.images.ImagesPipeline': 102
+	'sat8.MyImagesPipeline.MyImagesPipeline' : 102,
 	#'sat8.ConverImagePipeline.ConverImagePipeline' : 102,
 	# 'sat8.YoutubePipeline.YoutubePipeline' : 101,
 }
