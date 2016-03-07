cd2974286f7b65bf80ec30b804eb98bbb0793dc1
justin <cong.itsoft@gmail.com>
2016-03-07 13:53:39 +0700

    youtube

--- sat8/YoutubePipeline.py
+++ sat8/YoutubePipeline.py
@@ -100,7 +100,7 @@ class YoutubePipeline(object):
          })
 
    def getVideosByProducts(self):
-      self.cursor.execute("SELECT DISTINCT video_keyword FROM products WHERE video_keyword != '' OR video_keyword != NULL")
+      self.cursor.execute("SELECT DISTINCT video_keyword FROM products WHERE price > 0 AND video_keyword != '' OR video_keyword != NULL")
       products = self.cursor.fetchall()
 
       for product in products:
