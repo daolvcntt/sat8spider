c9210a93b86757af94835c855cc4fb890954247d
justin <cong.itsoft@gmail.com>
2016-07-11 16:57:25 +0700

    patch download image missed

---
+++ sat8/PatchDownloadImageFromUrl.py
@@ -0,0 +1,50 @@
+from scrapy.conf import settings
+from sat8.Helpers.Functions import *
+from sat8.Helpers.Google_Bucket import *
+
+images = [
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/180x350/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-white_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-black_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-blue_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-gold_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-navy_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-white_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-black-back_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-gold-back.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/v/1/v10-white-back_1.png',
+
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver_1_2.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gold_1_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gray_1_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver_1_2.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gold-back_1_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gray-back_1_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver-back_1_1.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-1_1_1.jpg',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-3_1_1.jpg',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/thumbnail/60x60/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-2_1_1.jpg',
+
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/180x350/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver_1_2.png',
+
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/180x350/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gold_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gold_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gray_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gold-back_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-gray-back_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-silver-back_4.png',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-1_2.jpg',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-2_2.jpg',
+    'http://cellphones.com.vn/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/n/o/note-3-3_2.jpg'
+]
+
+for image in images:
+    print image + "\n"
+    imageName = sha1FileName(image)
+
+    thumbs = downloadImageFromUrl(image)
+
+    # Upload bucket
+    google_bucket_upload_object('static.giaca.org', thumbs['full'], 'uploads/full/' + imageName)
+    google_bucket_upload_object('static.giaca.org', thumbs['big'], 'uploads/thumbs/big/' + imageName)
+    google_bucket_upload_object('static.giaca.org', thumbs['small'], 'uploads/thumbs/small/' + imageName)
