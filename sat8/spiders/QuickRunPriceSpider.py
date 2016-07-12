477976b91f199a5d93e097ce1c31e0e1b3396261
justin <cong.itsoft@gmail.com>
2016-07-12 12:50:53 +0700

    quick run price spider

---
+++ sat8/spiders/QuickRunPriceSpider.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from DbCrawlPriceV2Spider import DbCrawlPriceV2Spider
+
+class QuickRunPriceSpider(DbCrawlPriceV2Spider):
+
+    env = 'quick'
--- sat8/spiders/QuickRunProductSpiderV2.py
+++ sat8/spiders/QuickRunProductSpiderV2.py
@@ -5,9 +5,3 @@ from ProductSpiderV2 import ProductSpiderV2
 class QuickRunProductSpiderV2(ProductSpiderV2):
 
     env = 'quick'
-
-    def parse_detail_content(self, response):
-        yield self.get_product(response)
-
-    def parseJsonDetailContent(self, response):
-        yield self.getProductJson(response)
